386d85780cc0d07e241fd9a7cf722952c178f1e0
David Rodriguez Souto <drodsou@gmail.com>
2018-04-16 18:34:14 +0200

    firstly

---
+++ REAME.md
@@ -0,0 +1,40 @@
+converts urls to pdf file
+
+
+# dist standalone executable requirements
+
+'chrome.exe' must be in the PATH
+
+# example 
+
+```
+node url2pdf https://www.google.com 
+
+or
+
+url2pdf-win64 https://www.google.com
+
+```
+
+or
+
+```
+node url2pdf text-file-with-urls {"landscape":true}
+```
+
+# parameters
+
+  1: url or tex-file-with-urls
+  
+  2: JSON with page.pdf parameters. See:
+   https://github.com/GoogleChrome/puppeteer/blob/master/docs/api.md#pagepdfoptions
+   
+ 
+
+# output
+
+outputs to current dir if single url or to text-file-with-urls directory
+
+# format of text-file-with-urls
+
+just an url per line, \n 
---
+++ url2pdf.js
@@ -0,0 +1,71 @@
+const puppeteer = require('puppeteer')
+const fs = require('fs')
+const path = require('path')
+
+
+;(async ()=>{   // ; is important!
+
+  // process url parameter
+  let param = process.argv[2]
+  if (!param) {
+    console.log('ERROR: I need one paramenter: the url of the page, or the path to a text file with a list of urls')
+    process.exit(-1)
+  }
+
+  // possible argument option
+  // docs: https://github.com/GoogleChrome/puppeteer/blob/master/docs/api.md#pagepdfoptions
+  pagePdfOptions = {
+    format: 'A4',
+    landscape:false, 
+    printBackground:true
+  }
+  let paramOptions = process.argv.slice(3).map(e=>e.toLowerCase())
+  if (paramOptions.includes('landscape')) { pagePdfOptions.landscape = true }
+  if (paramOptions.includes('a3')) { pagePdfOptions.format = 'A3' }
+
+  console.log('page.pdf options:', pagePdfOptions)
+
+  // build array of urls
+  let outputDir
+  let urls
+  if (param.startsWith('http')) {
+    // asume parameter is a single url
+    outputDir = process.cwd()
+    urls = [param]
+
+  }
+  else  {
+    // asume parameter is path to a file
+    outputDir = path.dirname(param) // the same folder of passed urls file
+    let fileContents = fs.readFileSync(param, 'UTF8')
+    urls = fileContents.split('\n')
+  }
+ 
+
+  // enter puppeteer, genearte pdf for each 'urls' element
+  const browser = await puppeteer.launch( {
+    //executablePath: './node_modules/puppeteer/.local-chromium/win64-549031/chrome-win32/chrome.exe', // needed for pkg
+    //executablePath: 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe', // needed for pkg
+    executablePath: 'chrome', // needs to be in PATH
+    headless: true
+  } )
+  var page
+  //let url = 'http://localhost:3000/#printReport?nome=%22JORGE%22&apelidos=%22ALVAREZ%20SEVILLA%22&nif=%2252932334L%22&modulos=%5B%22%22%5D&titulo=%22Arquitectura%20sobre%20Amazon%20Web%20Services%20(AWS%20Solutions%20Architect%20Associate)%22&dataInicio=%2226%2F02%2F2018%22&dataFin=%2209%2F03%2F2018%22&horaInicio=%2216%3A30%22&horaFin=%2220%3A30%22&dataSinatura=%2226%20de%20febreiro%20de%202018%22&tipoEdicion=%22TIPED_2%22&codEdicion=%22XTIFCT1702%22&totalHorasPrograma=40&idActividadeFormativa=%22406102%22&temario=null&report=%22Diploma%22'
+  for (let url of urls) {
+    page = await browser.newPage()
+    
+    console.log('• pdfing', url)    
+    await page.goto(url, {waitUntil: 'networkidle2'})
+    
+    console.log('--- goto done')    
+    let pageTitle = await page.title()
+    pagePdfOptions.path = path.join(outputDir, pageTitle + '.pdf')
+    
+    console.log('--- writing pdf:', pagePdfOptions.path)    
+    await page.pdf(pagePdfOptions)
+
+    await page.close()
+  }
+  await browser.close()
+
+})()
